4484c300bea68f109e16a0d61450f70a2cc7e539
akshit316 <97580678+akshit316@users.noreply.github.com>
2023-05-03 11:54:42 +0530

    Create ceaser.py

---
+++ ceaser.py
@@ -0,0 +1,29 @@
+def encrypt(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    print(result)
+    return result
+
+
+def decrypt(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) - shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) - shift - 97) % 26 + 97)
+        else:
+            result += char
+    print(result)
+    return result
+
+encrypt("Hello world", 3)
+decrypt("Khoor zruog", 3)
